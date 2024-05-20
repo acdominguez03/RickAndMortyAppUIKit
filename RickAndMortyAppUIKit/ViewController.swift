eba4699ba5fbccb8f581293ae520405427e696e0
acdominguez03 <acordondominguez31@gmail.com>
2024-05-20 11:30:51 +0200

    Initial Commit

---
+++ RickAndMortyAppUIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickAndMortyAppUIKit
+//
+//  Created by Andres Cordón on 20/5/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
